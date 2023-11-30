066d48e40a55c87ff6cd38d202d8d9a859874e7a
woodstock <nonaandnea@yahoo.com>
2023-11-29 19:12:47 -0800

    Ops Challenge 02 done!

---
+++ OpsChallengeClass03.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Prompt for directory path
+read -p "Enter any directory path: " dir
+
+# Prompt for permissions number
+read -p "Enter permissions number: " permissionsNum
+
+# Prompt for changing the directory as specified by the user
+cd $dirPath
+
+# For loop to iterate through all files within directory; using a "for" loop to process each file
+for file in *; do 
+    # Process each file
+done
+
+# Using chmod to change permission of each file to a number specified by user
+chmod $permissionsNum "$file"
+
+# Using the ls command to list contents of the directory, including the modified permissions
+ls -l
+
+# Resources used: https://bard.google.com/chat/78c8bcf10dc27f25
