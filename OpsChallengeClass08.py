0a60c7470d3102b03814cec37aeb4dfc9c087106
woodstock <nonaandnea@yahoo.com>
2023-12-06 21:13:51 -0800

    OpsChallenge 06 complete!

---
+++ OpsChallengeClass08.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# create a list
+
+#  index:     0       1        2       3      4        5       6       7           8           9       10
+my_list = ['junk', 'stuff', 'house', 'car', 'cat', 'window', 'tree', 'pipe', 'dragonfruit', 'grape', 'nuts']
+
+# print the entire list/array
+# print(my_list)
+
+# print the index 1 inside of the list/array called my_list
+# print(my_list[3])
+
+# Print the last item in the index?
+# print(my_list[-1])
+
+# Print the fourth element on the list
+print(my_list[3])
+
+# Print the sixth through tenth element of list
+print(my_list[5:])
+
+# Change the value of the seventh element to "onion"
+stuff[6] = "onion"
+
+Resources used: https://g.co/bard/share/4a41e35f1987
